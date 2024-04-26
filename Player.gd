6b7e77749caf5077dcc5577a2220b5ec00a71624
USER_NAME <berkayster@gmail.com>
2024-04-25 21:54:08 -0500

    Edit : collision detection

--- Player.gd
+++ Player.gd
@@ -4,8 +4,8 @@ extends RigidBody3D
 @export var torquePower : float = 0.0;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	_press_spacebar(_delta)
+func _process(delta: float) -> void:
+	_press_spacebar(delta)
 	pass
 
 func _press_spacebar(delta : float) -> void:
@@ -15,3 +15,10 @@ func _press_spacebar(delta : float) -> void:
 		apply_torque(Vector3(0.0, 0.0, torquePower * delta))
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -torquePower * delta))
+
+func _on_body_entered(body:Node) -> void:
+	if "Goal" in body.get_groups():
+		print("You Win!")
+	elif "Hazard" in body.get_groups():
+		print("Your Crashed!")
+	pass # Replace with function body.
