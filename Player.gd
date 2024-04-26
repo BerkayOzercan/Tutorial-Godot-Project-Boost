43dc167e86ba90c0ed048e5898eddd2d950778ed
USER_NAME <berkayster@gmail.com>
2024-04-25 20:24:09 -0500

    Edit : test custom input actions

--- Player.gd
+++ Player.gd
@@ -9,9 +9,9 @@ func _process(_delta: float) -> void:
 	pass
 
 func _press_spacebar(delta : float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * forcePower)
-	if Input.is_action_pressed("ui_right"):
-		apply_torque(Vector3(0.0, 0.0, -torquePower * delta))
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torquePower * delta))
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0.0, 0.0, -torquePower * delta))
